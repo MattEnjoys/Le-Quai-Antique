4118282016ebfa38e9476953d3377127f7915cb9
Matt Enjoy's - Niveau global <matthieu73100@orange.fr>
2023-05-07 14:53:39 +0200

    Commit initial: Tous les dossiers et fichiers.

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+//Carrousel de index.html
+let counter = 1;
+setInterval(function () {
+    document.getElementById("radio" + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 3000);
+
+
